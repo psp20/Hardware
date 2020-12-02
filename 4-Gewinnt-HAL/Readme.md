81deac9bfbf85ad5953af0b85ca10ddf6f46ba13
laurin <laurin.wolf@gmx.de>
2020-12-02 10:45:00 +0100

    remove HAL

--- 4-Gewinnt-HAL/Readme.md
+++
@@ -1,4 +0,0 @@
-# Vier Gewinnt Firmware
-
-The sourcecode for the firmware can be found in ./src
-The code written by the gamelogic team is copied over by using the copy_classes.py
--- 4-Gewinnt-HAL/src/Stein.cpp
+++
@@ -1,8 +0,0 @@
-#include "Stein.h"
-
-namespace V4
-{
-    Stein::Stein(SteinOwner _owner, uint8_t _row, uint8_t _col)
-        : owner(_owner), row(_row), col(_col) {}
-
-} // namespace V4
